8ebf222129f578c771b3e100dd9db4f633b1541e
YouproContact <tech@youprocontact.co.uk>
2022-11-27 07:40:10 +0200

    initialised project with spring initializr

---
+++ src/main/java/com/taps/dronesapi/DronesApiApplication.java
@@ -0,0 +1,13 @@
+package com.taps.dronesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronesApiApplication.class, args);
+	}
+
+}
